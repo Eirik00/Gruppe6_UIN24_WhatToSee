b183e5c8fb35bf5a773a76c76084ccd7ca3e48b9
Eirik00 <eirik.rene@gmail.com>
2024-05-20 19:43:37 +0200

    compare siden

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -32,9 +32,9 @@ export default function Categories({user}){
                 <h2>Sjangere</h2>
                 <ul>
                     {
-                    categories?.map((category, index)=><li key={index}><Link to={"/category/"+category.slug} >{category.categorytitle}</Link>
-                    {user?.favcategory?.some(e => e.slug === category.slug) ? <button onClick={() => leggTilFavorit(category._id)}><IoIosStar />Fjern fra favorittliste</button> : <button onClick={() => leggTilFavorit(category._id)}><IoIosStarOutline  />  Legg til i favorittliste</button>}</li>)
-                 }
+                        categories?.map((category, index)=><li key={index}><Link to={"/category/"+category.slug} >{category.categorytitle}</Link>
+                        {user?.favcategory?.some(e => e.slug === category.slug) ? <button onClick={() => leggTilFavorit(category._id)}><IoIosStar />Fjern fra favorittliste</button> : <button onClick={() => leggTilFavorit(category._id)}><IoIosStarOutline  />  Legg til i favorittliste</button>}</li>)
+                    }
                 </ul>
             </main>
         </Layout>
--- frontend/src/components/Compare.jsx
+++ frontend/src/components/Compare.jsx
@@ -1,35 +1,56 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Layout from "./Layout";
 import { useEffect, useState } from "react";
 import { fetchUserById } from "../../sanity/services/usersServices";
+import MovieCard from "./MovieCard";
 
 export default function Compare({user}){
     const {slug} = useParams()
     const [comparedUser, setComparedUser] = useState(null)
-
+    const [wishlist, setWishList] = useState([])
+    const [favlist, setFavList] = useState([])
+    const [favCat, setFavCat] = useState([])
 
     const getComparedUser = async ()=>{
         const data = await fetchUserById(slug)
         setComparedUser(data[0])
-        console.log(data[0])
+
+        setLists()
+    }
+    const setLists = () =>{
+        setWishList(user?.wishlist?.filter(a => comparedUser?.wishlist?.some(b => a._id === b._id)))
+        setFavList(user?.favmovie?.filter(a => comparedUser?.favmovie?.some(b => a._id === b._id)))
+        setFavCat(user?.favcategory?.filter(a => comparedUser?.favcategory?.some(b => a._id === b._id)))
     }
 
     useEffect(()=>{
         getComparedUser()
-    },[slug])
+    },[user])
     return(
         <Layout user={user}>
             <main>
                 <h2>Forslag for {user?.username} og {comparedUser?.username}</h2>
-                <section>
+                <section id="comparePage">
                     <article>
-                        <h3></h3>
+                        <h3>Cathc up!</h3>
+                        <p>Dere har {wishlist?.length} filmer felles i ønskelisten deres.</p>
+                        <section>
+                            {wishlist?.map((movie, index) => <MovieCard key={index} movie={movie} />)}
+                        </section>
                     </article>
                     <article>
-                        
+                        <h3>Go safe!</h3>
+                        <p>Dere har {favlist?.length} film felles i favorittlisten deres.</p>
+                        <section>
+                            {favlist?.map((movie, index) => <MovieCard key={index} movie={movie} />)}
+                        </section>
                     </article>
                     <article>
-                        
+                        <h3>Utforsk!</h3>
+                        <p>Dere begge liker disse sjangerne, sjekk hvilke filmer som finnes å velge mellom:</p>
+                        <ul>
+                            {favCat?.map((category, index) => <li><Link key={index} to={"/category/"+category.slug}>{category.categorytitle}</Link></li>)}
+                        </ul>
                     </article>
                 </section>
             </main>
